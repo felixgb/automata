52cfb99264528be6ea3ae6b46a620bdfd70c32d9
felixgb <felixgbowman@gmail.com>
2016-11-07 23:47:21 +0000

    string matching function

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 
 "use strict";
 
-const program = require('commander');
+const program = require("commander");
+
+// csv parser
+const Papa = require("babyparse");
+const fs = require("fs");
 
 function configFromArgs() {
     program
@@ -15,16 +19,77 @@ function configFromArgs() {
         .option("-t, --num-steps <nsteps>", "Time steps to run for")
         .parse(process.argv);
 
+    loadRules(program)
+}
+
+function loadRules(parsedArgs) {
+    let path = parsedArgs.rulesPath;
+    let content = fs.readFileSync(path, { encoding: "binary" });
+
+    Papa.parse(content, {
+        skipEmptyLines: true,
+        complete: function(res) {
+            runAtomata(res, parsedArgs);
+        }
+    });
+}
+
+function makeConfig(rules, parsedArgs) {
+    let ruleTable = rules.data.map(ruleFromRow);
+
     return {
-        size: program.size,
-        nsize: program.neighborhoodSize,
-        istate: program.initState,
-        path: program.rulesPath,
-        nsteps: program.numSteps
+        size: parsedArgs.size,
+        nsize: parseInt(parsedArgs.neighborhoodSize),
+        istate: parsedArgs.initState,
+        ruleTable: ruleTable,
+        nsteps: parsedArgs.numSteps
     };
 }
 
-console.log(configFromArgs());
+function runAtomata(rules, parsedArgs) {
+    let config = makeConfig(rules, parsedArgs);
+    let current = config.istate;
 
+    for (let i = 0; i < config.nsteps; i++) {
+        current = step(config.nsize, current, config.ruleTable);
+        console.log(current);
+    }
+}
+
+function step(nsize, current, ruleTable) {
+    let next = "";
+    // Come up with a non idiot way of doing this
+    for (let i = 0; i < current.length; i++) {
+        let lpad = (i - nsize) < 0 ? "0".repeat(nsize - i) : "";
+        let rpad = (i + nsize) > current.length ? "0".repeat(i - current.lenth) : "";
+        let c = lpad + current + rpad;
+        console.log((i - nsize) + lpad.length, (i + nsize) + lpad.length);
+        let toCompare = c.slice((i - nsize) + lpad.length, (i + nsize) + lpad.length);
+        next += matchRule(toCompare, ruleTable);
+    }
+    return next;
+}
+
+function matchRule(current, ruleTable) {
+    for (let i = 0; i < ruleTable.length; i++) {
+        let row = ruleTable[i].elems;
+
+        console.log(row);
+        console.log(current);
+        if (row === current) {
+            return ruleTable[i].next;
+        }
+    }
+    throw "no rule matched!";
+}
+
+function ruleFromRow(row) {
+    return {
+        next: row.slice(-1).pop(),
+        elems: row.slice(0, -1).join("")
+    }
+}
+
+configFromArgs();
 // if (program.args.length === 0) program.help();
 
