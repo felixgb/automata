2c25160940d1ac139bc118987f9dec140c4191bd
felixgb <felixgbowman@gmail.com>
2016-11-08 16:43:31 +0000

    changed step function, more tests

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
 #!/usr/bin/env node
 
+"use strict";
+
+const program = require("commander");
+const Papa = require("babyparse");
+const fs = require("fs");
+
 const automata = require("./src/automata");
 
-automata.main();
+function loadRules(parsedArgs) {
+    const path = parsedArgs.rulesPath;
+    const content = fs.readFileSync(path, { encoding: "binary" });
+
+    Papa.parse(content, {
+        skipEmptyLines: true,
+        complete: function(res) {
+            let config = makeConfig(res, parsedArgs);
+            validate(config);
+            automata.runAtomata(config);
+        }
+    });
+}
+
+function validate(config) {
+    const neighborhoodSize = config.nsize * 2 + 1;
+    const equalToNsize = (row) => row.length === neighborhoodSize;
+    const rules = Object.keys(config.ruleTable);
+
+    if (!rules.every(equalToNsize)) {
+        throw "Rule table row size does not match neighbor size";
+    }
+    if (rules.length != Math.pow(2, neighborhoodSize)) {
+        throw "Not enough rules for the given neighbor size!";
+    }
+}
+
+function makeConfig(rules, parsedArgs) {
+    const ruleTable = makeRuleTable(rules.data);
+
+    return {
+        size: parseInt(parsedArgs.size),
+        nsize: parseInt(parsedArgs.neighborhoodSize),
+        istate: parsedArgs.initState,
+        ruleTable: ruleTable,
+        nsteps: parseInt(parsedArgs.numSteps)
+    };
+}
+
+function makeRuleTable(rules) {
+    return rules.reduce(function(ruleTable, row) {
+        const elems = row.slice(0, -1).join("");
+        const next = row.slice(-1).pop();
+
+        ruleTable[elems] = next;
+        return ruleTable;
+    }, {});
+}
+
+(function main() {
+    program
+        .version('0.0.1')
+        .description("Simple cellular automata")
+        .option("-s, --size <size>","Size of the automata")
+        .option("-n, --neighborhood-size <nsize>", "Neighborhood size")
+        .option("-i, --init-state <istate>", "Initial state")
+        .option("-p, --rules-path <path>", "Path to rule table")
+        .option("-t, --num-steps <nsteps>", "Time steps to run for")
+        .parse(process.argv);
+
+    loadRules(program);
+})();
--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
+"use strict";
+
 const should = require("should");
+
 const automata = require("../src/automata");
 
-describe("Test repeat take", function() { 
-
-    "use strict";
-
+describe("Test repeatTake", function() { 
     it("repeatedly takes string slice", function() {
         let str = "000110100110";
         let res = [
@@ -22,5 +22,40 @@ describe("Test repeat take", function() {
         //  "000110100110";
         ];
         automata.repeatTake(3, str).should.deepEqual(res);
+    });
+});
+
+describe("Test matchRule", function() { 
+    it("matches a rule", function() {
+        let current = "010";
+        let table = {
+            "000": "0",
+            "001": "0",
+            "010": "0",
+            "011": "1",
+            "100": "0",
+            "101": "1",
+            "110": "1",
+            "111": "0"
+        };
+        automata.matchRule(current, table).should.equal("0");
+    });
+});
+
+describe("Test step", function() { 
+    it("matches a rule", function() {
+        let current = "0011010011";
+        let next = "0011100011";
+        let table = {
+            "000": "0",
+            "001": "0",
+            "010": "0",
+            "011": "1",
+            "100": "0",
+            "101": "1",
+            "110": "1",
+            "111": "0"
+        };
+        automata.step(1, current, table).should.equal(next);
     });
 });
